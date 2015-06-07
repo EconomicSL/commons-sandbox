84e6e8e08c6d485f67a35099bafc50a4dfe2415d
David R. Pugh <drobert.pugh@gmail.com>
2015-06-07 18:54:56 +0100

    Added additional docs to contracts.primitives package.

--- src/main/scala-2.11/plumbing/contracts/primitives/package.scala
+++ src/main/scala-2.11/plumbing/contracts/primitives/package.scala
@@ -3,11 +3,17 @@ package plumbing.contracts
 /** Classes for constructing composable contracts.
   *
   * ==Overview==
-  * Instead of defining a fixed catalogue of financial contracts, we want to
-  * define a fixed set of "building blocks" that can be used to modelers to
-  * construct commonly encountered financial contracts (i.e., stocks, bonds,
-  * deposits, secured and unsecured loans, etc), but that could also be used
-  * actors within a model to innovate new, unforeseen financial contracts.
+  * Instead of defining a fixed catalogue of tradable financial contracts, our
+  * approach is to instead define a small set of primitive contracts that can
+  * be used as "building blocks" to construct more complicated contracts.
+  *
+  * We envision that:
+  *
+  * 1. model builders will use these primitive contracts to construct commonly
+  * encountered financial contracts such as stocks, bonds, deposits, secured
+  * and unsecured loans, etc.
+  * 2. actors within a model will use these primitive contracts to innovate new,
+  * unforeseen financial contracts.
   *
   * ===Composable contracts===
   * Implement the approach from [[https://lexifi.com/files/resources/MLFiPaper.pdf Peyton-Jones et al (2000)]].
