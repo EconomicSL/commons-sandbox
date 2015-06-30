55e5c4d3f2171356575ce4ce78caa61279e31d28
David R. Pugh <drobert.pugh@gmail.com>
2015-06-30 23:16:11 +0100

    Added options to cp and tried to fix directory paths.

--- .travis/publish_docs.sh
+++ .travis/publish_docs.sh
@@ -17,8 +17,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
   cd gh-pages
   git rm -rf ./docs
   git rm -rf ./coverage
-  cp ../target/scala-2.11/api ./docs/api/latest
-  cp ../target/scala-2.11/scoverage-report ./coverage/scoverage-report
+  cp -Rf $HOME/target/scala-2.11/api ./docs/api/latest
+  cp -Rf $HOME/target/scala-2.11/scoverage-report ./coverage/scoverage-report
   git add -f .
   git commit -m "Lastest doc and coverage report on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
