50d7416d78b899c8dbce2358522c3287234a1fa7
David R. Pugh <drobert.pugh@gmail.com>
2015-07-01 12:47:14 +0100

    Added OAuth token so that Travis can push to gh-pages.

--- .travis/publish_docs.sh
+++ .travis/publish_docs.sh
@@ -14,17 +14,20 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/inet-oxford/commons-sandbox gh-pages > /dev/null
 
   cd gh-pages
-  
+
+  # get rid of old docs and coverage stats
   git rm -rf ./docs
   git rm -rf ./coverage
-  ls -aF
 
+  # copy over the new docs
   mkdir -p ./docs/api/latest
   cp -Rf ../target/scala-2.11/api/* ./docs/api/latest
 
+  # copy over the new coverage stats
   mkdir -p ./coverage
   cp -Rf ../target/scala-2.11/scoverage-report ./coverage
 
+  # push to github!
   git add -f .
   git commit -m "Lastest doc and coverage report on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
