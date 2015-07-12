7dfa1bd5396e88c225fa1a59742a6fd204c52c94
David R. Pugh <drobert.pugh@gmail.com>
2015-07-12 15:30:20 +0100

    Updated the docs for the acl and PromiseMakingActor.

--- src/main/scala-2.11/acl/PromiseMakingActor.scala
+++ src/main/scala-2.11/acl/PromiseMakingActor.scala
@@ -1,75 +1,90 @@
 /*
 Copyright 2015 David R. Pugh, Dan F. Tang, J. Doyne Farmer
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+the License. You may obtain a copy of the License at
 
 http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 */
 package acl
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import edsl.commitments.Commitment
 
-import scala.collection.immutable
+import scala.collection.mutable
 
 
-/** Trait defining behavior for an actor that trades [[edsl.commitments.Commitment Commitment]] objects. */
+/** Trait defining behavior for a `PromiseMakingActor`. */
 trait PromiseMakingActor {
   this: Actor with ActorLogging =>
 
-  /** The act of accepting a previously submitted [[Promise]] to perform
-    * actions specified in some [[edsl.commitments.Commitment Commitment]].
-    *
-    * @param promise a [[Promise]] containing a [[edsl.commitments.Commitment Commitment]] specifying certain actions
-    *                that the `PromiseMakingActor` is to perform.
-    * @note `acceptPromise` is a general-purpose acceptance of a [[Promise]]
-    *       that was previously submitted (typically via a `makePromise` act).
-    *       The `PromiseMakingActor` sending [[PromiseAccepted]] is informing the
-    *       receiver that it intends to perform the actions as specified in
-    *       the [[edsl.commitments.Commitment Commitment]].
+  /** Collection of [[acl.Promise `Promise`]] messages accepted by the `PromiseMakingActor`. */
+  def acceptedPromises: mutable.Set[Promise]
+
+  /** Collection of [[acl.Promise `Promise`]] messages sent by the `PromiseMakingActor` and accepted by some other 
+    * `PromiseMakingActor`.
     */
-  def acceptPromise(promise: Promise): Unit = {
+  def outstandingPromises: mutable.Set[Promise]
+
+  /** The act of informing another `PromiseMakingActor` that a previously received [[acl.Promise `Promise`]] has been
+    * accepted.
+    *
+    * @param promise A [[acl.Promise `Promise`]] message.
+    * @note `accept` is a general-purpose acceptance of a [[acl.Promise `Promise`]] message that was previously
+    *       submitted (typically via a `make` act). The `PromiseMakingActor` sending
+    *       [[acl.PromiseAccepted `PromiseAccepted`]] is informing the receiver that it intends to perform the actions
+    *       as specified in the [[edsl.commitments.Commitment `Commitment`]].
+    */
+  def accept(promise: Promise): Unit = {
+    acceptedPromises += promise
     promise.sender ! PromiseAccepted(self, promise.receiver, promise)
   }
 
-  /** The action of submitting a [[Promise]] made by the `PromiseMakingActor` to a
-    * collection of other `PromiseMakingActors` to perform actions specified in the
-    * [[edsl.commitments.Commitment Commitment]].
-    *
-    * @param receiver a collection of `PromiseMakingActors` who are receiving the
-    *                 [[Promise]] and therefore are potential counter parties to
-    *                 the [[edsl.commitments.Commitment Commitment]].
-    * @param contract a [[edsl.commitments.Commitment Commitment]] specifying certain actions that the various
-    *                 parties should perform.
-    * @note `makePromise` is a general-purpose act to make a [[Promise]] or
-    *       respond to a previously submitted [[Promise]] (i.e., during a
-    *       negotiation process) by promising to perform certain actions
-    *       defined by the [[edsl.commitments.Commitment Commitment]].
+  /** The act of informing another `PromiseMakingActor` that a previously accepted [[acl.Promise `Promise`]] has been
+    * cancelled.
+    * 
+    * @param promise A [[acl.Promise `Promise`]] message.
+    * @note The [[acl.PromiseCancelled `PromiseCancelled`]] message should only be sent in response to a
+    *       [[acl.CancelPromise `CancelPromise`]] message.
     */
-  def makePromise(receiver: immutable.Set[ActorRef], contract: Commitment): Unit = {
-    receiver.foreach(r => r ! Promise(self, receiver, contract))
+  def cancel(promise: Promise): Unit = {
+    acceptedPromises -= promise
+    promise.sender ! PromiseCancelled(self, promise.sender, promise)
   }
 
-  /** The action of rejecting a previously submitted [[Promise]] to perform
-    * actions specified in some [[edsl.commitments.Commitment Commitment]].
+  /** The act of making a [[acl.Promise `Promise`]] to another `PromiseMakingActor`.
     *
-    * @param promise a [[Promise]] containing a [[edsl.commitments.Commitment Commitment]] specifying certain
-    *                actions that the `PromiseMakingActor` is to perform.
-    * @note `rejectPromise` is a general-purpose rejection to a [[Promise]]
-    *       that was previously submitted (typically via a `makePromise` act).
-    *       The `PromiseMakingActor` sending [[PromiseRejected]] is informing the
-    *       receiver that it has no intention to perform the actions as
-    *       specified in the promised [[edsl.commitments.Commitment Commitment]].
+    * @param commitment A [[edsl.commitments.Commitment `Commitment`]] specifying certain actions that the various
+    *                   parties should perform.
+    * @param receiver A `PromiseMakingActor` who is receiving the [[acl.Promise `Promise`]].
+    * @note `make` is a general-purpose act to make a [[acl.Promise `Promise`]] or respond to a previously submitted
+    *      [[acl.Promise `Promise`]] (i.e., during a negotiation process) by promising to perform certain actions
+    *      defined by the [[edsl.commitments.Commitment `Commitment`]].
+    */
+  def make(commitment: Commitment, receiver: ActorRef): Unit = {
+    receiver ! Promise(self, receiver, commitment)
+  }
+
+  /** The act of informing another `PromiseMakingActor` that a previously received [[acl.Promise `Promise`]] has been
+    * rejected.
+    *
+    * @param promise A [[acl.Promise `Promise`]] message.
+    * @note `rejectPromise` is a general-purpose rejection to a [[acl.Promise `Promise`]] that was previously submitted
+    *       (typically via a `make` act). The `PromiseMakingActor` sending [[acl.PromiseRejected `PromiseRejected`]]
+    *       is informing the receiver that it has no intention to perform the actions as specified in the underlying
+    *       [[edsl.commitments.Commitment `Commitment`]].
     */
   def rejectPromise(promise: Promise): Unit = {
     promise.sender ! PromiseRejected(self, promise.receiver, promise)
   }
+
+  def promiseMakingBehavior: Receive = {
+    case CancelPromise(sender, receiver, promise) => cancel(promise)
+    case PromiseAccepted(sender, receiver, promise) => outstandingPromises += promise
+  }
+
 }
--- src/main/scala-2.11/acl/package.scala
+++ src/main/scala-2.11/acl/package.scala
@@ -1,121 +1,89 @@
 /*
 Copyright 2015 David R. Pugh, Dan F. Tang, J. Doyne Farmer
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+the License. You may obtain a copy of the License at
 
 http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 */
 import akka.actor.ActorRef
-import acl.{Promise, PromiseMakingActor}
-import edsl.{ContractActorLike, CounterpartyActor}
+import acl.PromiseMakingActor
 import edsl.commitments.Commitment
 
-import scala.collection.immutable
 
-
-/** Provides classes defining an economic actor as well as a high-level economic
-  * actor communication language (ACL).
+/** Provides classes defining a high-level economic actor communication language (ACL).
   *
   * ==Overview==
-  * "To analyze how financial commitments affect the economy it is necessary to
-  * look at economic units in terms of their cash flows. The cash-flow approach
-  * looks at all units -- be they households, corporations, state and municipal
-  * governments, or even national governments -- as if they were banks."
+  * The `acl` package defines a high-level actor communication language (ACL) as well as the
+  * [[PromiseMakingActor `PromiseMakingActor`]] behavioral trait that an actor must implement in order to
+  * communicate using the ACL protocol.
   *
-  * The above quotation is taken from Hyman Minsky's magnum opus ''Stabilizing
-  * an Unstable Economy''.  Following Minsky, and more recently Perry Mehrling,
-  * we view every economic actor as an entity that is both receiving certain
-  * cash flow events (i.e., receipts of various kinds) as well as generating
-  * cash flow events (i.e., expenditures of various kinds) over time.
-  *
-  * The accumulation of cash flow events for a particular economic actor is
-  * captured by its balance sheet. Most basic constraint that economic actors face in our framework is, to
-  * again borrow from Minsky, the survival constraint: the inflow of cash flow
-  * events must be at least as big as the outflow of generated cash flow
-  * events.  By taking the "banking perspective" our primary concern is the
-  * liquidity of economic actors; solvency of economic actors is a secondary
-  * concern.
-  *
-  * Anytime an economic actor needs to make a payment it has only three
-  * options:
-  *
-  *  1. Spend down hoards of money.
-  *
-  *  2. Liquidate accumulations of financial assets.
-  *
-  *  3. Increase accumulations of financial liabilities.
-  *
-  * Crucially both 2 and 3 depend on finding some other economic actor to take
-  * the other side of the transaction. This may be easier said than done.  Only
-  * option 1 is completely dependable.  Thus the import of money.
-  *
-  *
-  * ===`PromiseMaker`===
-  * The `acl` package defines the [[PromiseMakingActor]] trait as well as the
-  * high-level [[PromiseMakingActor]] communication language.
-  *
-  * Our high-level [[PromiseMakingActor]] acl language is influenced by, but not slave
-  * to, the [[http://www.fipa.org/ Foundation for Intelligent Physical Agents (FIPA)]]
-  * compliant [[http://www.fipa.org/specs/fipa00037/SC00037J.pdf Agent Communication Language (ACL)]].
+  * === Actor Communication Language ===
+  * Our high-level [[PromiseMakingActor `PromiseMakingActor`]] actor communication language is influenced by, but not
+  * slave to, the [[http://www.fipa.org/ Foundation for Intelligent Physical Agents (FIPA)]] compliant
+  * [[http://www.fipa.org/specs/fipa00037/SC00037J.pdf Agent Communication Language (ACL)]].
   *
   * A [[PromiseMakingActor]] communicates with other [[PromiseMakingActor]] actors by making
-  * [[acl.Promise promises]] which are then either [[acl.PromiseAccepted accepted]]
-  * or [[acl.PromiseRejected rejected]] by the receiving [[PromiseMakingActor]] actors.
+  * [[acl.Promise Promise]] messages which are then either [[acl.PromiseAccepted accepted]] or
+  * [[acl.PromiseRejected rejected]], or countered by the receiving [[PromiseMakingActor]] actors.
+  *
+  * A [[PromiseMakingActor `PromiseMakingActor`]] that wishes to cancel a previously accepted promise can do so by
+  * sending the [[acl.CancelPromise `CancelPromise`]] message.
+  *
+  * ===`PromiseMakingActor`===
+  *
+  *
   */
 package object acl {
 
-  /** A message indicating that a [[CounterpartyActor]] actor has broken an
-    * existing [[edsl.commitments.Commitment Commitment]].
+  /** A message indicating that a previously accepted [[Promise `Promise`]] should be cancelled.
     *
-    * @param sender the [[CounterpartyActor]] actor breaking the existing
-    *               [[edsl.commitments.Commitment Commitment]].
-    * @param receiver the [[ContractActorLike]] actor representing the existing
-    *                 [[edsl.commitments.Commitment Commitment]] who's terms
-    *                 are being breached.
+    * @param sender The [[PromiseMakingActor `PromiseMakingActor`]] cancelling the `promise`.
+    * @param receiver The [[PromiseMakingActor `PromiseMakingActor`]] who previously accepted the `promise`.
+    * @param promise The previously accepted [[Promise `Promise`]] which should be cancelled.
     */
-  case class ContractBroken(sender: ActorRef, receiver: ActorRef)
+  case class CancelPromise(sender: ActorRef, receiver: ActorRef, promise: Promise)
 
-
-  /** A promise made by some [[PromiseMakingActor]] to a collection of other
-    * [[PromiseMakingActor]] edsl to perform actions specified in the
-    * [[edsl.commitments.Commitment Commitment]].
+  /** A `Promise` is a message from some [[PromiseMakingActor `PromiseMakingActor`]] to some other
+    * [[PromiseMakingActor `PromiseMakingActor`]] which proposes that both the actors perform certain actions as
+    * specified in the message `content`.
     *
-    * @param sender the [[PromiseMakingActor]] making the proposal.
-    * @param receiver a collection of [[PromiseMakingActor]] edsl who are receiving the
-    *                 [[Promise]] and therefore are potential counter parties to
-    *                 the [[edsl.commitments.Commitment Commitment]].
-    * @param contract a [[edsl.commitments.Commitment Commitment]] specifying
-    *                 certain actions that the sender and receiver should perform.
+    * @param sender The [[PromiseMakingActor `PromiseMakingActor`]] sending the `Promise`.
+    * @param receiver The [[PromiseMakingActor `PromiseMakingActor`]] receiving the `Promise`.
+    * @param content A [[edsl.commitments.Commitment `Commitment`]] specifying certain actions that the `sender` and
+    *                `receiver` should perform given certain preconditions.
+    * @param inReplyTo A previously submitted `Promise` to which the current `Promise` is in reply to.
     */
-  case class Promise(sender: ActorRef, receiver: immutable.Set[ActorRef], contract: Commitment)
+  case class Promise(sender: ActorRef, receiver: ActorRef, content: Commitment, inReplyTo: Option[Promise] = None)
 
-  /** A message indicated that a previously submitted [[Promise]] has been accepted.
+  /** A message indicating that a previously received [[Promise `Promise`]] has been accepted.
     *
-    * @param sender the [[PromiseMakingActor]] accepting the previously received [[Promise]].
-    * @param receiver a collection of [[PromiseMakingActor]] edsl who are receiving the
-    *                 [[Promise]] and therefore are potential counter parties to
-    *                 the [[edsl.commitments.Commitment Commitment]].
-    * @param promise the previously received [[Promise]] that is being accepted.
-
+    * @param sender The [[PromiseMakingActor `PromiseMakingActor`]] accepting the [[Promise `Promise`]]
+    * @param receiver The [[PromiseMakingActor `PromiseMakingActor`]] who originally sent the [[Promise `Promise`]].
+    * @param inReplyTo The previously received `Promise` to which the `PromiseAccepted` message is in reply to.
     */
-  case class PromiseAccepted(sender: ActorRef, receiver: immutable.Set[ActorRef], promise: Promise)
+  case class PromiseAccepted(sender: ActorRef, receiver: ActorRef, inReplyTo: Promise)
 
-  /** A message indicating that a previously submitted [[Promise]] has been rejected.
+  /** A message indicating that a previously accepted [[Promise `Promise`]] has been cancelled.
+    *
+    * @param sender The [[PromiseMakingActor `PromiseMakingActor`]] cancelling the `promise`.
+    * @param receiver The [[PromiseMakingActor `PromiseMakingActor`]] who originally sent the `promise`.
+    * @param inReplyTo The previously accepted `Promise` message which has been cancelled.
+    */
+  case class PromiseCancelled(sender: ActorRef, receiver: ActorRef, inReplyTo: Promise)
+
+  /** A message indicating that a previously received [[Promise]] has been rejected.
     *
     * @param sender the [[PromiseMakingActor]] rejecting the previously received [[Promise]].
     * @param receiver a collection of [[PromiseMakingActor]] edsl who are receiving the
     *                 [[Promise]] and therefore are potential counter parties to
     *                 the [[edsl.commitments.Commitment Commitment]].
-    * @param promise the previously received [[Promise]] that is being rejected.
+    * @param promise the previously received [[Promise]] that has been rejected.
     */
-  case class PromiseRejected(sender: ActorRef, receiver: immutable.Set[ActorRef], promise: Promise)
+  case class PromiseRejected(sender: ActorRef, receiver: ActorRef, promise: Promise)
 
 }
