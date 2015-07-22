9de17c0227aa40b9c068cb54d201c5b8eeeb2a84
David R. Pugh <drobert.pugh@gmail.com>
2015-07-22 13:18:25 +0100

    Finalized the documentation for the Agree communicative act.

--- src/main/scala-2.11/acl/acts/Agree.scala
+++ src/main/scala-2.11/acl/acts/Agree.scala
@@ -19,16 +19,16 @@ import acl.Beliefs
 
 /** A message sent from some [[acl.CommunicatingActor `CommunicatingActor`]] (i.e., `sender`) to another
   * [[acl.CommunicatingActor `CommunicatingActor`]] (i.e., `receiver`) indicating that the `sender` has agreed to
-  * perform some action(s) as specified in a [[acl.acts.Request `Request`]] message previously sent by the `receiver`.
+  * perform some action(s) as specified in a [[acl.acts.Request `Request`]] message (ie., `request`) previously sent by
+  * the `receiver`.
   *
-  * @param conversationId is used to identify an ongoing sequence of [[acl.acts.CommunicativeAct `CommunicativeAct`]]
-  *                       messages that together form a conversation.
-  * @param request is the previously received [acl.Request `Request`] message that has been agreed.
+  * @param conversationId is used to identify a sequence of [[acl.acts.CommunicativeAct `CommunicativeAct`]] messages
+  *                       that together form a conversation.
+  * @param request is the previously received `request` that has been agreed.
   * @param predicate defines a precondition that should be satisfied in order for the `sender` to perform the
   *                  action(s) specified in the agreed `request`.
   * @tparam A is the type of action expression used to construct the content of the `request`.
-  * @note The `Agree` message is sent by a [[acl.CommunicatingActor `CommunicatingActor`]] using the
-  *       [[acl.CommunicatingActor.agree `agree`]] action.
+  * @note The `Agree` message is sent by `sender` using the [[acl.CommunicatingActor.agree `agree`]] action.
   */
 case class Agree[A](conversationId: UUID,
                     request: Request[A],
